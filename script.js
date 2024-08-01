d4cc95d7387dc6d8482a15907bc11a1343299b09
Sholeh Konsultan Perizinan Usaha <108649772+sholehperizinan@users.noreply.github.com>
2024-08-01 12:18:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,34 @@ document.addEventListener("keydown", e => {
 		e.preventDefault();
 	};
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// --------------- remove attributes ---------------
+document.addEventListener('DOMContentLoaded', function() {
+        // URL src skrip yang ingin dihapus
+        var scriptToRemove = 'https://www.blogger.com/static/v1/widgets/2613211189-widgets.js';
+        
+        // Temukan semua elemen <script>
+        var scripts = document.getElementsByTagName('script');
+
+        // Iterasi melalui elemen-elemen <script>
+        for (var i = 0; i < scripts.length; i++) {
+            // Periksa apakah src dan type dari elemen <script> cocok dengan yang dicari
+            if (scripts[i].getAttribute('src') === scriptToRemove && scripts[i].getAttribute('type') === 'text/javascript') {
+                // Hapus elemen <script> yang sesuai
+                scripts[i].parentNode.removeChild(scripts[i]);
+                console.log('Skrip dengan src "' + scriptToRemove + '" telah dihapus.');
+                break; // Hentikan pencarian setelah elemen ditemukan dan dihapus
+            }
+        }
+    });
